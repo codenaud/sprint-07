c8fbe20d9d0b87b563bedd55f58a09ed3ae8972a
codenaud <codenaud@gmail.com>
2024-02-09 13:04:34 +0100

    detail data working

--- src/app/pages/starship-details/starship-details.component.ts
+++ src/app/pages/starship-details/starship-details.component.ts
@@ -1,4 +1,4 @@
-//starsgip-details.component.ts
+// starship-details.component.ts
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { StarWarsService } from '../../shared/api/starwars.service';
@@ -13,34 +13,20 @@ import { StarWarsService } from '../../shared/api/starwars.service';
 export class StarshipDetailsComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private starwarsService = inject(StarWarsService);
-  starships: any;
+  starship: any;
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.starwarsService.getStarshipById(id).subscribe((starship) => {
-        this.starships = starship;
+        // Asegúrate de que aquí procesas correctamente la respuesta para extraer la información de la nave estelar
+        // y asignar la URL de la imagen adecuadamente
+        const imageUrl = this.starwarsService.getStarshipsImageUrl(id);
+        this.starship = { ...starship, imageUrl: imageUrl };
       });
     }
-    this.loadStarships();
   }
 
-  loadStarships() {
-    this.starwarsService.getStarships().subscribe((response: any) => {
-      this.starships = response.results.map((starship: any) => {
-        const id = this.extractId(starship.url); // Extraes el ID
-        const imageUrl = this.starwarsService.getStarshipsImageUrl(id); // Extraer la imágen
-        return {
-          ...starship,
-          id: id,
-          imageUrl: imageUrl, // Añades la URL de la imagen al objeto de la nave estelar
-        };
-      });
-      console.log(this.starships); // Ahora deberías ver las naves estelares con URLs de imágenes en la consola
-    });
-  }
-
-  // Si no hay imagen se mostrará esta imagen
   onImageError(event: any) {
     event.target.src =
       'https://starwars-visualguide.com/assets/img/big-placeholder.jpg';
